a6868324a752601a9803a43982ac5ba5be484c20
Kevin Wang <kevinwang@Kevins-MBP-2.attlocal.net>
2022-04-12 19:36:49 -0700

    created contacts array

---
+++ index.js
@@ -0,0 +1,23 @@
+const contacts = [
+    {
+        name: 'kevin',
+        email: 'kw1017@gmail.com',
+        phone: 8582055523
+    },
+    {
+        name: 'Braxxton',
+        email: 'kw1017@gmail.com',
+        phone: 8582055523
+    }
+];
+
+console.log(contacts);
+
+// Display all existing contacts
+function displayContacts() {
+
+}
+
+// Display/Render the existing contacts
+// How to use the input values and create that contact object
+// How to push that object into the contacts array
