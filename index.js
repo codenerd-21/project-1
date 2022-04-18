6085d4b1357befa4f22dfc3ec2c5f6069c52db88
Braxton Toomer <braxton.toomer1@gmail.com>
2022-04-17 20:57:02 -0700

    render contacts

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 const contacts = [
     {
-        name: 'kevin',
+        name: 'Braxton',
+        email: 'braxton.toomer1@gmail.com',
+        phone: 7075834446
+    },
+    {
+        name: 'Kevin',
         email: 'kw1017@gmail.com',
         phone: 8582055523
     },
-    {
-        name: 'Braxxton',
-        email: 'kw1017@gmail.com',
-        phone: 8582055523
-    }
 ];
 
-console.log(contacts);
+document.getElementById('display-contacts').innerHTML = JSON.stringify(contacts);
+
+
+
+// window.onload = function() {
+// 	document.getElementById('display-contacts').innerHTML = Object.values(contacts);
+// }
+// document.getElementById("display-contacts").innerHTML = contacts.name;
 
 // Display all existing contacts
-function displayContacts() {
-
-}
+// function displayContacts() {
+// 	let book = document.getElementById('display.contacts');
+// 	book.textContent = contacts;
+// }
 
 // Display/Render the existing contacts
 // How to use the input values and create that contact object
